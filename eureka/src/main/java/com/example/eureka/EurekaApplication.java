1b58641c381bca2e37d0f6bffde03bb67f94680e
nhiennd <nhiennd@vnext.com.vn>
2022-12-15 09:02:31 +0700

    update config prometheus, grafana UI

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
 
 	public static void main(String[] args) {
+		String ip = InetAddress.getLoopbackAddress().getHostAddress();
+		System.out.println("ip: " + ip);
 		SpringApplication.run(EurekaApplication.class, args);
 	}
 
