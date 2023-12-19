ec2e7051be5a42300bc02d73eee77f05d85b253b
nhiennd <nhiennd@vnpt.vn>
2023-12-19 15:08:39 +0700

    update service name

--- product-service/src/test/java/com/example/product/ProductApplicationTests.java
+++ product-service/src/test/java/com/example/product/ProductApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.eureka;
+package com.example.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaApplicationTests {
+class ProductApplicationTests {
 
 	@Test
 	void contextLoads() {
